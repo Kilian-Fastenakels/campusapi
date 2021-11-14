3187348b0b51c31ff668b2781ea83f541c4bf68a
Kilian-Fastenakels <kilian.fastenakels@thomasmore.be>
2021-11-14 21:24:28 +0100

    final

--- src/routes.js
+++ src/routes.js
@@ -23,6 +23,38 @@ router.get('/', (req, res) => {
     +'Returns all campuses in the database using .find()'
 
     +'<hr/>'
+
+    +'<h2>/campus/:id</h2>'
+    +'<p>Returns one single campus in the database using .findById(objectId)</p>'
+    +'<p>Uses req.params.id, which means the id is passed in the url</p>'
+    +'<p>/campus/6191576ea6578c26ce2cdbc1 returns the campus with id 6191576ea6578c26ce2cdbc1</p>'
+    
+    +'<hr/>'
+
+    +'<h2>/campus/create</h2>'
+    + '<p>Adds one single campus to the database using .create({data})</p>'
+    + '<p>Uses req.body, which means an object is passed</p>'
+    + '<p>/api/campus/add adds the campus to the database</p>'
+    + '<p>Since we can\'t pass a body in our browser, this path only works when called via code/postman</p>'
+    
+    +'<hr/>'
+    
+    +'<h2>/campus/update/:id</h2>'
+    + '<p>Updates one single campus in the database using .findByIdAndUpdate(objectId, { $set: <data> })</p>'
+    + '<p>Uses req.params.id, which means the id is passed in the url</p>'
+    + '<p>Also uses req.body, which means an object is passed</p>'
+    + '<p>/campus/update/6191576ea6578c26ce2cdbc1 updates the campus with 6191576ea6578c26ce2cdbc1 with the data passed in req.body</p>' 
+    + '<p>Since we can\'t pass a body in our browser, this path only works when called via code/postman</p>'
+    
+    +'<hr/>'
+
+    +'<h2>/campus/delete/:id</h2>'
+    + '<p>Deletes one single campus from the database using .findByIdAndDelete(objectId)</p>'
+    + '<p>Uses req.params.id, which means the id is passed in the url</p>'
+    + '<p>/campus/delete/6191576ea6578c26ce2cdbc1 deletes the campus with id 6191576ea6578c26ce2cdbc1 from the database</p>'
+    + '<p><strong>Please only delete campus you added</strong></p>'
+    
+    +'<hr/>'
   );
 });
 
